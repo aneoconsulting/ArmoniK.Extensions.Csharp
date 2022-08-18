85c4a818494cc8466bd7475b40189c31a6ddd18d
Dylan Brasseur <dbrasseur@aneo.fr>
2022-08-18 11:32:58 +0200

    Removed streamwrappertests (already in core), modified tests to work. End to end test doesn't compile => waiting to test separate api packages for protos

--- Common/StreamWrapper/tests/Server/build.sh
+++
@@ -1,9 +0,0 @@
-trap "vim -c :q" ERR EXIT
-
-set -e
-
-mkdir -p /tmp/worker
-rm -rf /tmp/worker/*
-
-dotnet publish -c Release Common/StreamWrapper/tests/Server/ArmoniK.Extensions.Common.StreamWrapper.Tests.Server.csproj -o /tmp/worker
-docker build -t dockerhubaneo/armonik_worker_streamworker_test:test -f Common/StreamWrapper/tests/Server/Dockerfile.Copy /tmp/worker
