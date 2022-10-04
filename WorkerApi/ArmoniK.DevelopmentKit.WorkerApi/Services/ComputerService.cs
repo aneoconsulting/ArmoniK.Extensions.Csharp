ad029e3a8d3ab5d8839e7072fe23c49f31c43c7b
Dylan Brasseur <dbrasseur@aneo.fr>
2022-10-04 15:33:48 +0200

    Fixing assembly loading checks

--- WorkerApi/ArmoniK.DevelopmentKit.WorkerApi/Services/ComputerService.cs
+++ WorkerApi/ArmoniK.DevelopmentKit.WorkerApi/Services/ComputerService.cs
@@ -155,13 +155,13 @@ public class ComputerService : WorkerStreamWrapper
       Logger.LogError(ex,
                       "WorkerAPIException failure while executing task");
 
-      output = new Output
-               {
-                 Error = new Output.Types.Error
-                         {
-                           Details = ex.Message + Environment.NewLine + ex.StackTrace,
-                         },
-               };
+      return new Output
+             {
+               Error = new Output.Types.Error
+                       {
+                         Details = ex.Message + Environment.NewLine + ex.StackTrace,
+                       },
+             };
     }
 
     catch (Exception ex)
