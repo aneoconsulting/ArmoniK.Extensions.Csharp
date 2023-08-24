d8c22b5a6c180fb2d8b89039e2d653c46989acb1
Wilfried Kirschenmann <17836122+wkirschenmann@users.noreply.github.com>
2023-08-24 22:02:58 +0200

    Correct BlockRequest

--- Client/src/Unified/Services/Submitter/BlockRequest.cs
+++ Client/src/Unified/Services/Submitter/BlockRequest.cs
@@ -29,8 +29,10 @@ internal class BlockRequest
 
   public ArmonikPayload? Payload { get; set; }
 
-  public SemaphoreSlim Lock        { get; set; }
-  public Guid          SubmitId    { get; set; }
-  public int           MaxRetries  { get; set; } = 5;
-  public TaskOptions   TaskOptions { get; set; }
+  public SemaphoreSlim Lock     { get; set; }
+  public Guid          SubmitId { get; set; }
+
+  public string      ResultId    { get; set; }
+  public int         MaxRetries  { get; set; } = 5;
+  public TaskOptions TaskOptions { get; set; }
 }
