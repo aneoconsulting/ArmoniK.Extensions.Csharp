4a355e007bfcf6e8d117769aa67c7165b058a00e
jeremyzyngeraneo <jzynger@aneo.fr>
2023-09-04 12:24:29 +0200

    fix: handle request if error

--- tools/logs2seq.py
+++ tools/logs2seq.py
@@ -14,7 +14,7 @@ import os
 
 
 def is_valid_file(name: str) -> bool:
-    if name.find("control") != -1 and name.endswith(".log"):
+    if name.find("compute") != -1 and name.endswith(".log"):
         return True
     return False
 
@@ -28,50 +28,46 @@ def filter_logs(file_list: list[str]) -> list[str]:
     return result
 
 
-def make_post_request(url, data):
+def make_post_request(url: str, data: str):
     try:
         resp = requests.post(url, data=data)
         resp.raise_for_status()
+        return resp.content.decode("utf-8"), False
+    except requests.HTTPError as e:
+        return f"Une erreur s'est produite lors de la requête : {e}", True
+    except ...:
+        return f"Une erreur s'est produite et n'a pas été gérée", True
 
-        return resp.content.decode("utf-8")
 
-    except requests.exceptions.RequestException as e:
-        return f"Une erreur s'est produite lors de la requête : {e}"
+def send_line(line: str, url: str):
+    try:
+        json_data: dict = json.loads(line)
+        log_message = json_data.get("log")
+        if log_message:
+            message, error = make_post_request(url, log_message)
+            if error:
+                logger.error(message)
+                sys.exit(84)
+            logger.debug(log_message)
+    except json.JSONDecodeError:
+        pass
 
 
 def send_log_file(name: str, file_obj: tarfile.TarFile, url: str):
-    batch = 0
     ctr = 0
-    tosend = ""
     file = file_obj.extractfile(name)
     for line in file:
         line = line.decode("utf-8").strip()
         if line.startswith("{"):
-            try:
-                json_data = json.loads(line)
-                # if "log" not in json_data:
-                #     continue
-                log_message = json_data.get("log")
-                tosend += log_message
-                logging.debug(log_message)
-            except json.JSONDecodeError:
-                pass
-        if batch > 1000:
-            make_post_request(url, tosend)
-            tosend = ""
-            batch = 0
-        batch = batch + 1
-        ctr = ctr + 1
-    logging.info(f"sent : {ctr}",)
-    if tosend != "":
-        make_post_request(url, tosend)
+            send_line(line, url)
+            ctr += 1
+    logger.info(f"sent : {ctr}")
 
 
 def extract_jsontar_log(url: str, file_name: str):
     file_obj = tarfile.open(file_name, "r")
     file_list = file_obj.getnames()
     file_list = filter_logs(file_list)
-    # logging.debug(file_list)
     for file in file_list:
         send_log_file(file, file_obj, url)
 
@@ -81,7 +77,7 @@ def extract_jsontar_log(url: str, file_name: str):
 if __name__ == "__main__":
 
     # Création du logger
-    logger = logging.getLogger(__file__)
+    logger = logging.getLogger(Path(__file__).name)
     logging.basicConfig(
         # Définit le niveau à partir du quel les logs sont affichés
         level=logging.INFO
