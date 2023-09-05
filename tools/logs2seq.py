01c7989f69155f0ce060082ef9bcb6449db84dcd
jeremyzyngeraneo <jzynger@aneo.fr>
2023-09-05 15:01:43 +0200

    chore: change tosend batch in bytes

--- tools/logs2seq.py
+++ tools/logs2seq.py
@@ -1,3 +1,4 @@
+from typing import IO
 import requests
 import json
 import argparse
@@ -12,6 +13,13 @@ import os
 # docker rm -f seqlogpipe
 # docker run -d --rm --name seqlogpipe -e ACCEPT_EULA=Y -p 9080:80 -p 9341:5341 datalust/seq
 
+# Création du logger
+logger = logging.getLogger(Path(__file__).name)
+logging.basicConfig(
+    # Définit le niveau à partir du quel les logs sont affichés
+    level=logging.INFO
+)
+
 VALID_LOG_FILES = [
     'control',
     'compute'
@@ -24,74 +32,43 @@ def is_valid_file(name: str) -> bool:
     return False
 
 
-def filter_logs(file_list: list[str]) -> list[str]:
-    result = []
-    # file:str
-    for file in file_list:
-        if is_valid_file(file):
-            result.append(file)
-    return result
-
-
 def make_post_request(url: str, data: str):
-    try:
-        resp = requests.post(url, data=data)
-        resp.raise_for_status()
-        return resp.content.decode("utf-8"), False
-    except requests.HTTPError as e:
-        return f"Une erreur s'est produite lors de la requête : {e}", True
-    except requests.exceptions.ConnectionError as e:
-        return f"Une erreur s'est produite lors de la requête : {e}", True
+    logger.debug(f"send : {len(data)} bytes")
+    requests.post(url, data=data)
+    # logger.info(resp.json())
+    # # resp.raise_for_status()
+    # return resp.content.decode("utf-8"), False
 
 
-def send_batch(data: str, url: str):
-    message, error = make_post_request(url, data)
-    if error:
-        logger.error(message)
-        sys.exit(84)
-    logger.debug(data)
-
-
-def send_log_file(name: str, file_obj: tarfile.TarFile, url: str):
+def send_log_file(file: IO[bytes], url: str):
     ctr = 0
-    batch = 0
-    tosend = ""
-    file = file_obj.extractfile(name)
+    tosend = b""
     for line in file:
         line = line.decode("utf-8").strip()
         if line.startswith("{"):
+            ctr = ctr + 1
             json_data = json.loads(line)
-            log_message = json_data.get("log")
-            tosend += log_message
-        if batch > 100:
-            send_batch(tosend, url)
-            tosend = ""
-            batch = 0
-        batch = batch + 1
-        ctr = ctr + 1
+            log_message = bytes(json_data.get("log"), "utf-8")
+
+            if len(tosend) + len(log_message) > 100000:
+                make_post_request(url, tosend)
+                tosend = log_message
+            else:
+                tosend += log_message
+
     logger.info(f"sent : {ctr}")
-    if tosend != "":
-        send_batch(tosend, url)
+    if tosend != b"":
+        make_post_request(url, tosend)
 
 
 def extract_jsontar_log(url: str, file_name: str):
-    file_obj = tarfile.open(file_name, "r")
-    file_list = file_obj.getnames()
-    file_list = filter_logs(file_list)
-    for file in file_list:
-        send_log_file(file, file_obj, url)
-
-    file_obj.close()
+    with tarfile.open(file_name, "r") as file_obj:
+        for file in file_obj.getnames():
+            if is_valid_file(file):
+                send_log_file(file_obj.extractfile(file), url)
 
 
 if __name__ == "__main__":
-
-    # Création du logger
-    logger = logging.getLogger(Path(__file__).name)
-    logging.basicConfig(
-        # Définit le niveau à partir du quel les logs sont affichés
-        level=logging.INFO
-    )
 
     parser = argparse.ArgumentParser(description=" Local ArmoniK logs in JSON CLEF format then send them to Seq.",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -100,8 +77,8 @@ if __name__ == "__main__":
                         default="http://localhost:9341/api/events/raw?clef")
     args = parser.parse_args()
 
-    filename = "/home/jeremyzynger/test_logs/end2end-false-false-disable-false.tar"
-
+    # filename = "/home/jeremyzynger/test_logs/end2end-false-false-disable-false.tar"
+    filename = "/home/jeremyzynger/armonik/ArmoniK.Core/test.tar.gz"
     if not Path(filename).exists():
         logger.critical(f"ERROR : file does not exist {filename}")
 
