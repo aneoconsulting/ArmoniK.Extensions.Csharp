43b9b4e23c83a0896e9009ce1f6f47cdc77ebacd
jeremyzyngeraneo <jzynger@aneo.fr>
2023-09-13 15:40:30 +0200

    refactor: use fonction parameters instead of global variable

--- tools/logs2seq.py
+++ tools/logs2seq.py
@@ -16,18 +16,19 @@ logging.basicConfig(
 )
 
 
-def is_valid_file(name: str) -> bool:
+def is_valid_file(valid_log_files: list[str], name: str) -> bool:
     """
     Select a specific file or folder based on name
 
     Args:
+        valid_log_files: List of valid files
         name: The file or folder name to be checked
 
     Returns:
         bool: True if the file name ends with ".log" and match with the names
         defined as valid, False otherwise
     """
-    return any(([name.endswith(".log") and log_file in name for log_file in args.valid_log_files]))
+    return any(([name.endswith(".log") and log_file in name for log_file in valid_log_files]))
 
 
 def make_post_request(url: str, data: str):
@@ -36,6 +37,7 @@ def make_post_request(url: str, data: str):
 
     Args:
         url: The URL to which the POST request will be made
+        valid_log_files: List of valid files
         data: The data to be sent in the POST request body
     """
     logger.debug(f"send : {len(data)} bytes")
@@ -70,7 +72,7 @@ def send_log_file(file: str, url: str):
         make_post_request(url, tosend)
 
 
-def extract_jsontar_log(url: str, file_name: str):
+def extract_jsontar_log(url: str, valid_log_files: list[str], file_name: str):
     """
     Extract log data from a tar archive and send to a seq server
 
@@ -80,11 +82,11 @@ def extract_jsontar_log(url: str, file_name: str):
     """
     with tarfile.open(file_name, "r") as file_obj:
         for file in file_obj.getnames():
-            if is_valid_file(file):
+            if is_valid_file(valid_log_files, file):
                 send_log_file(file_obj.extractfile(file), url)
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(description="Download ArmoniK logs in tar format from S3 bucket then send them to Seq.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("bucket_name", help="S3 bucket", type=str)
     parser.add_argument("folder_name", help="Folder where extcsharp logs are located", type=str)
@@ -92,11 +94,12 @@ if __name__ == "__main__":
     parser.add_argument("run_attempt", help="GitHub workflow run_attempt", type=str)
     parser.add_argument("file_name", help="file to download from the bucket", type=str)
     parser.add_argument("--url", dest="url", help="Seq url", type=str, default="http://localhost:9341/api/events/raw?clef")
-    parser.add_argument("--file", dest="valid_log_files", nargs='+', help="List of choosen files", default=['control-plane','compute-plane'])
+    parser.add_argument("--files", dest="valid_log_files", nargs='+', help="List of choosen files", default=['control-plane','compute-plane'])
     args = parser.parse_args()
-    
+
     tmp_dir = "./tmp/"
-    dir_name = os.path.join(args.folder_name, args.run_number, args.run_attempt)
+    dir_name = os.path.join(
+        args.folder_name, args.run_number, args.run_attempt)
     obj_name = os.path.join(dir_name, args.file_name)
     file_name = os.path.join(tmp_dir, obj_name)
 
@@ -105,4 +108,8 @@ if __name__ == "__main__":
     s3 = boto3.client('s3')
     s3.download_file(args.bucket_name, obj_name, file_name)
 
-    extract_jsontar_log(args.url, file_name)
+    extract_jsontar_log(args.url, args.valid_log_files, file_name)
+
+
+if __name__ == "__main__":
+    main()
