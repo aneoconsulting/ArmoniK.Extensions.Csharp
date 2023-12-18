e98f2d2a3367beb16651b151ebd130f758748f4b
Jérôme Gurhem <jgurhem@aneo.fr>
2023-12-18 07:45:07 +0100

    fix: retry on RpcException in envents waitforresults

--- Client/src/Common/Submitter/EventsClientExt.cs
+++ Client/src/Common/Submitter/EventsClientExt.cs
@@ -1,19 +1,18 @@
 // This file is part of the ArmoniK project
-//
-// Copyright (C) ANEO, 2021-$CURRENT_YEAR$. All rights reserved.
-//
+// 
+// Copyright (C) ANEO, 2021-2023. All rights reserved.
+// 
 // Licensed under the Apache License, Version 2.0 (the "License")
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
 
 using System;
 using System.Collections.Generic;
@@ -25,6 +24,8 @@ using ArmoniK.Api.gRPC.V1;
 using ArmoniK.Api.gRPC.V1.Events;
 using ArmoniK.Api.gRPC.V1.Results;
 using ArmoniK.DevelopmentKit.Client.Common.Exceptions;
+
+using Grpc.Core;
 
 namespace ArmoniK.DevelopmentKit.Client.Common.Submitter;
 
@@ -76,7 +77,8 @@ internal static class EventsClientExt
                                                                         resultsNotFound.Select(ResultsFilter),
                                                                       },
                                                                     },
-                                                 });
+                                                 },
+                                                 cancellationToken: cancellationToken);
       try
       {
         while (await streamingCall.ResponseStream.MoveNext(cancellationToken))
@@ -117,7 +119,10 @@ internal static class EventsClientExt
           }
         }
       }
-      catch (OperationCanceledException e)
+      catch (OperationCanceledException)
+      {
+      }
+      catch (RpcException)
       {
       }
     }
