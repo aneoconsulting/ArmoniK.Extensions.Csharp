a9696641d672b8db1899db5ca2895a7e4d84877f
Florian Lemaitre <flemaitre@aneo.fr>
2024-06-04 10:18:28 +0200

    Fixed headers

--- Client/src/Common/Submitter/ChannelPool.cs
+++ Client/src/Common/Submitter/ChannelPool.cs
@@ -1,13 +1,13 @@
 // This file is part of the ArmoniK project
-//
-// Copyright (C) ANEO, 2021-2023. All rights reserved.
-//
+// 
+// Copyright (C) ANEO, 2021-2024. All rights reserved.
+// 
 // Licensed under the Apache License, Version 2.0 (the "License")
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
