c479ef21ff423867374e37ff0a38fd14b19cbee0
Florian Lemaitre <flemaitre@aneo.fr>
2025-06-16 18:03:47 +0200

    feat: implem Health Check

--- Worker/src/Symphony/ServiceContainerBase.cs
+++ Worker/src/Symphony/ServiceContainerBase.cs
@@ -1,13 +1,13 @@
 // This file is part of the ArmoniK project
-// 
+//
 // Copyright (C) ANEO, 2021-2023. All rights reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License")
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -307,6 +307,9 @@ public abstract class ServiceContainerBase
   public void ConfigureSessionService(ITaskHandler taskHandler)
     => SessionService = new SessionPollingService(LoggerFactory,
                                                   taskHandler);
+
+  public virtual bool CheckHealth()
+    => true;
 }
 
 /// <summary>
@@ -373,4 +376,5 @@ public static class ServiceContainerBaseExt
                                                          IEnumerable<string>                           taskIds,
                                                          Func<IEnumerable<Tuple<string, byte[]>>, int> func)
     => throw new NotImplementedException();
+
 }
